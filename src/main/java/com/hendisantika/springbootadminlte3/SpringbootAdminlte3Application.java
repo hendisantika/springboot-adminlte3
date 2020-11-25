4854955bac82516ea49ceeefac2e6236549c1fce
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-25 08:36:32 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootadminlte3/SpringbootAdminlte3Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootadminlte3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminlte3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminlte3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootadminlte3/SpringbootAdminlte3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootadminlte3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminlte3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
