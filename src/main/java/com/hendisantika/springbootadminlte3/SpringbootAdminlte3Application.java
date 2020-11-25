ef5ebd5c8c30c7626727293b3bb205da98ea447c
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-25 08:39:47 +0700

    #4 - Add SpringBootServletInitializer in Main App

--- src/main/java/com/hendisantika/springbootadminlte3/SpringbootAdminlte3Application.java
+++ src/main/java/com/hendisantika/springbootadminlte3/SpringbootAdminlte3Application.java
@@ -2,12 +2,18 @@ package com.hendisantika.springbootadminlte3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootAdminlte3Application {
+public class SpringbootAdminlte3Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAdminlte3Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootAdminlte3Application.class);
+    }
 }
